b8caac053e8720eb37e7910b4f0c4e3c581d4c3e
fabiwoller <fabianwoller@web.de>
2026-06-12 20:37:30 +0200

    Update unit tests.

--- tests/test_core.py
+++ tests/test_core.py
@@ -324,3 +324,73 @@ class AdditionalGRNInferences(TestCase):
         to_feature_importances("LASSO",
                            LASSO_KWARGS,
                            reg)
+    
+    def test_svr(self):
+        target = np.array([0.5, 0.6, 0.2, 0.8])
+        tf_matrix = np.array([[0.5, 0.6],
+                              [0.3, 0.9],
+                              [1.2, 4.5],
+                              [6.7, 2.4]])
+        reg = fit_model("SVR",
+              SVR_KWARGS,
+              tf_matrix,
+              target,
+              early_stop_window_length=EARLY_STOP_WINDOW_LENGTH,
+              seed=DEMON_SEED)
+
+        to_feature_importances("SVR",
+                           SVR_KWARGS,
+                           reg)
+    
+    def test_ridge(self):
+        target = np.array([0.5, 0.6, 0.2, 0.8])
+        tf_matrix = np.array([[0.5, 0.6],
+                              [0.3, 0.9],
+                              [1.2, 4.5],
+                              [6.7, 2.4]])
+        reg = fit_model("RIDGE",
+              RIDGE_KWARGS,
+              tf_matrix,
+              target,
+              early_stop_window_length=EARLY_STOP_WINDOW_LENGTH,
+              seed=DEMON_SEED)
+
+        to_feature_importances("RIDGE",
+                           RIDGE_KWARGS,
+                           reg)
+        
+    def test_elastic(self):
+        target = np.array([0.5, 0.6, 0.2, 0.8, -1.3, 1.2])
+        tf_matrix = np.array([[0.5, 0.6],
+                              [0.3, 0.9],
+                              [1.2, 4.5],
+                              [6.7, 2.4],
+                              [-1.5, 2.3],
+                              [0.2, -0.2]])
+        reg = fit_model("ELASTIC",
+              ELASTIC_KWARGS,
+              tf_matrix,
+              target,
+              early_stop_window_length=EARLY_STOP_WINDOW_LENGTH,
+              seed=DEMON_SEED)
+
+        to_feature_importances("ELASTIC",
+                           ELASTIC_KWARGS,
+                           reg)
+    
+    def test_omp(self):
+        target = np.array([0.5, 0.6, 0.2, 0.8])
+        tf_matrix = np.array([[0.5, 0.6],
+                              [0.3, 0.9],
+                              [1.2, 4.5],
+                              [6.7, 2.4]])
+        reg = fit_model("OMP",
+              OMP_KWARGS,
+              tf_matrix,
+              target,
+              early_stop_window_length=EARLY_STOP_WINDOW_LENGTH,
+              seed=DEMON_SEED)
+
+        to_feature_importances("OMP",
+                           OMP_KWARGS,
+                           reg)
